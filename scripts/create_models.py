eb604386548c330dba9c3638ba3437283078d32e
Shunta Saito <shunta.saito@gmail.com>
2015-03-18 23:51:03 +0900

    modify lmdb path in model seeds

--- scripts/create_models.py
+++ scripts/create_models.py
@@ -26,7 +26,7 @@ def data_layer(number, bottom, object_type):
   top: "input_data"
   data_param {{
     backend: LMDB
-    source: "../../data/vmnih/mass_{object_type}/{dataset}/train_sat.lmdb"
+    source: "../../data/mass_{object_type}/{dataset}/train_sat.lmdb"
     batch_size: {batch_size}
   }}
   include: {{ phase: TRAIN }}
@@ -37,7 +37,7 @@ layer {{
   top: "input_label"
   data_param {{
     backend: LMDB
-    source: "../../data/vmnih/mass_{object_type}/{dataset}/train_map.lmdb"
+    source: "../../data/mass_{object_type}/{dataset}/train_map.lmdb"
     batch_size: {batch_size}
   }}
   include: {{ phase: TRAIN }}
@@ -48,7 +48,7 @@ layer {{
   top: "input_data"
   data_param {{
     backend: LMDB
-    source: "../../data/vmnih/mass_{object_type}/{dataset}/test_sat.lmdb"
+    source: "../../data/mass_{object_type}/{dataset}/test_sat.lmdb"
     batch_size: {batch_size}
   }}
   include: {{ phase: TEST }}
@@ -59,7 +59,7 @@ layer {{
   top: "input_label"
   data_param {{
     backend: LMDB
-    source: "../../data/vmnih/mass_{object_type}/{dataset}/test_map.lmdb"
+    source: "../../data/mass_{object_type}/{dataset}/test_map.lmdb"
     batch_size: {batch_size}
   }}
   include: {{ phase: TEST }}
--- shells/train.sh
+++ shells/train.sh
@@ -3,7 +3,7 @@ cd $1
 mkdir snapshots
 caffe_dir=$HOME/Libraries/caffe
 $caffe_dir/python/draw_net.py train_test.prototxt net.png
-export GLOG_log_dir=$PWD
+# export GLOG_log_dir=$PWD
 echo 'start learning' $1
 nohup $caffe_dir/build/tools/caffe train \
     -solver=$PWD/solver.prototxt &
