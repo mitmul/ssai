0997cc15a0efff8a3147eed7aeac2c3c52e0be11
Ubuntu <ubuntu@ip-172-31-26-0.ap-northeast-1.compute.internal>
2015-04-10 11:40:31 +0000

    fix train.sh

--- shells/train.sh
+++ shells/train.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
-cd $1
+fn=`date +"%Y-%m-%d_%I-%M-%S"`
+if [ ! -d results ]; then
+    mkdir results
+fi
+cd results
+fpath=$1
+modelname="${fpath##*/}"
+echo $modelname
+dname=$modelname'_'$fn
+echo $dname
+mkdir $dname
+cd $dname
 mkdir snapshots
+cp ../../models/$modelname/*.prototxt ./
 caffe_dir=$HOME/Libraries/caffe
 $caffe_dir/python/draw_net.py train_test.prototxt net.png
 # export GLOG_log_dir=$PWD
