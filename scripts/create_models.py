bbcc3fbc934951e86ca85a6f85f3e220bd5c8ef0
Shunta Saito <shunta.saito@gmail.com>
2015-04-12 23:01:48 +0900

    fix create_models

--- scripts/create_models.py
+++ scripts/create_models.py
@@ -446,7 +446,7 @@ if __name__ == '__main__':
                 if layer[0] == 'loss':
                     txt = globals()['predict_layer'](i, bottom, layer[1][0])
                 elif layer[0] == 'euclidean_loss':
-                    txt = globals()['predict_layer'](i, bottom, 'euclidean')
+                    txt = ''
                 else:
                     txt = globals()['%s_layer' % layer[0]](
                         i, bottom, *layer[1])
