83218f34a72851574436de32b2fcdce3a77675f9
Shunta Saito <shunta.saito@gmail.com>
2015-05-08 09:20:10 +0900

    remote

--- shells/validation.sh
+++ shells/validation.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-ssh crest-super-01 "cd Codes/ssai; python scripts/batch_evaluation.py"
 python scripts/collect_results.py
 python scripts/analize_results.py
 python scripts/comparing_curves.py
