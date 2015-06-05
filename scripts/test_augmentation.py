c871d0baaed9a2892acc87e520f012b9f8b59268
Shunta Saito <shunta.saito@gmail.com>
2015-06-05 16:50:48 +0900

    modify test_augmentation.py

--- scripts/test_augmentation.py
+++ scripts/test_augmentation.py
@@ -6,26 +6,28 @@ import cv2 as cv
 import caffe
 
 caffe.set_mode_gpu()
-caffe.set_device(0)
+caffe.set_device(8)
 
-model_fn = 'models/test_segmentation_data_solver.prototxt'
+model_fn = 'solver.prototxt'
 solver = caffe.SGDSolver(model_fn)
 
-# for t in range(2):
-#     solver.step(1)
+for t in range(2):
+    solver.step(1)
+
 output = solver.net.forward()
-
-for i in range(128):
-    data = output['data'][i]
-    label = output['label'][i]
-
-    data = data.transpose([1, 2, 0])
-    label = label.transpose([1, 2, 0])
-
-    ldata = np.copy(data)
-    ldata[data.shape[0] / 2 - label.shape[0] / 2:
-          data.shape[0] / 2 + label.shape[0] / 2,
-          data.shape[1] / 2 - label.shape[0] / 2:
-          data.shape[1] / 2 + label.shape[1] / 2, :] = label * 255
-    data = np.hstack([data, ldata])
-    cv.imwrite('%d.png' % i, data)
+print output
+#
+# for i in range(128):
+#     data = output['data'][i]
+#     label = output['label'][i]
+#
+#     data = data.transpose([1, 2, 0])
+#     label = label.transpose([1, 2, 0])
+#
+#     ldata = np.copy(data)
+#     ldata[data.shape[0] / 2 - label.shape[0] / 2:
+#           data.shape[0] / 2 + label.shape[0] / 2,
+#           data.shape[1] / 2 - label.shape[0] / 2:
+#           data.shape[1] / 2 + label.shape[1] / 2, :] = label * 255
+#     data = np.hstack([data, ldata])
+#     cv.imwrite('%d.png' % i, data)
